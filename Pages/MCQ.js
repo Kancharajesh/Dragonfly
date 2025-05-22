00c205b94949e54ca9b3f116b6f7d654dd2c7337
kancharajesh <rajeshkancha41@gmail.com>
2025-05-22 12:14:10 +0530

    mcq types

--- Pages/MCQ.js
+++ Pages/MCQ.js
@@ -14,14 +14,19 @@ export class MCQ{
     // Settings fields.
     this.S_MultipleSelection = page.locator("//span[normalize-space()='Multiple Selection']");
     
-
+    // Explanation True.
     this.S_Explain = page.locator("//span[normalize-space()='Explanation Field']");
-     
+    this.S_Explain_minchar = page.locator("(//input[@value='0'])[1]");
+    this.S_Explain_maxchar = page.locator("(//input[@value='0'])[2]");
+    this.S_Explain_Field1 = page.locator("(//input[contains(@placeholder,'Add your Question here')])[1]");
+    this.S_Explain_Field2 = page.locator("(//input[contains(@placeholder,'Add your Question here')])[2]");
+    this.S_Explain_Field_Cancel = page.locator("(//img[contains(@alt,'image')])[1]"); 
+
     // Description.
     this.S_Description = page.locator("//span[normalize-space()='Description Field']");
     this.S_Description_field = page.locator("(//div[contains(@class,'flex flex-col mb-[71.5px] gap-4 relative mr-[100px] ml-[88px] mt-[44px]')])[1]");
     this.S_Description_minChar= page.locator("(//input[@value='0'])[1]");
-    this.S_Description_maxChar = page.locator("(//input[@value='0'])[2]");
+    this.S_Description_maxChar = page.locator("(//input[@value='0'])[1]");
     this.S_Description_placeHolder = page.locator("//input[@placeholder='Describe your answer...']");
 
     this.S_Other = page.locator("//span[normalize-space()='Other']");
@@ -85,6 +90,30 @@ async Description_maxChar (text){
 
 async Description_placeHolder (text){
     await this.S_Description_placeHolder.fill(text);
+}
+
+async Explain_minChar(text){
+  await this.S_Explain_minchar.fill(text);
+}
+
+async Explain_maxchar(text){
+  await this.S_Explain_maxchar.fill(text);
+}
+
+async Explain_Filed_visible(){
+  await this.S_Explain_Field1.isVisible();
+}
+
+async Explain_field_1 (text){
+  await this.S_Explain_field1.fill(text);
+}
+
+async Explain_field_2 (text){
+  await this.S_Explain_Field2.fill(text);
+}
+
+async Explain_field_cancel (){
+  await this.S_Explain_Field_Cancel.click();
 }
 
 }
--- tests/Survey_creation.spec.js
+++ tests/Survey_creation.spec.js
@@ -20,6 +20,15 @@ test("Create Description Question", async ({ page }) => {
   await survey.fillQuestion("Title Slide Question text"); 
   await expect(survey.Question_input).toHaveValue("Title Slide Question text");
   await page.waitForTimeout(2000);
+  
+  // Settings.
+    await page.waitForTimeout(3000);
+    await Mcq.Description();
+    
+    await Mcq.Description_minChar("2");
+    await Mcq.Description_maxChar("10");
+    await Mcq.Description_placeHolder("Testing");
+    await page.waitForTimeout(5000);
 
 });
 
@@ -80,6 +89,41 @@ test("MCQ and Description opiton", async({page})=>{
     await Mcq.Description_maxChar("10");
     await Mcq.Description_placeHolder("Testing");
     await page.waitForTimeout(5000);
+
+});
+
+test("MCQ & Explanation ", async({page})=>{
+    const login = new Login(page);
+    const survey = new Survey(page);
+    const Mcq = new MCQ(page);
+
+    // Login
+    await login.doLogin("testdragon@gmail.com", "12345@6");
+    await login.HomeSurvey();
+    await page.waitForTimeout(5000);
+    await survey.A_MCQ();
+
+    // Fill Question and options text
+    await page.waitForTimeout(3000);
+    await survey.fillQuestion("multiple choice Question");
+    await expect(survey.Question_input).toHaveText("multiple choice Question");
+    await Mcq.MCQ_1("SuperJ");
+    await Mcq.MCQ_2("Jupiter");
+    await Mcq.MCQ_3("testing");
+    
+    // Settings.
+    await page.waitForTimeout(3000);
+    await Mcq.Explain();
+    await Mcq.Explain_Filed_visible();
+    
+    await Mcq.Explain_minChar("2");
+    await page.waitForTimeout(2000);
+    await Mcq.Description_maxChar("20");
+        await page.waitForTimeout(3000);
+
+    // await Mcq.Explain_field_1("Hey Hello How are you?");
+
+
 
 
 });
